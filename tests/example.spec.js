086dd8958b6e72881733ab4ed977c9f1814e7e22
maransugu9894-create <maransugu9894@gmail.com>
2026-09-24 15:58:40 +0530

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,8 @@
+import {test} from '@playwright/test';
+import { LoginPage} from '../pages/Login';
+import user from '../data/user-data.json'
+test('SauceDemo',async({page})=>{
+  const Login = new LoginPage(page);
+  await Login.login(user.user4.name,user.password);
+  await page.waitForTimeout(3000);
+})
